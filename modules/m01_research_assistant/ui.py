3775cf85a210edd4d1f993568a1371ce2bda10cc
Alnoor <alnoor606@gmail.com>
2026-06-25 13:29:07 -0400

    Restore bordered containers for Writers A and B

--- modules/m01_research_assistant/ui.py
+++ modules/m01_research_assistant/ui.py
@@ -376,9 +376,11 @@ div[data-baseweb="select"] * { cursor: pointer; }
     critic_gate_ph     = st.empty()
     _writers_cols      = st.columns(2)
     with _writers_cols[0]:
-        writer_a_ph    = st.empty()
+        with st.container(border=True):
+            writer_a_ph = st.empty()
     with _writers_cols[1]:
-        writer_b_ph    = st.empty()
+        with st.container(border=True):
+            writer_b_ph = st.empty()
     debate_judge_ph    = st.empty()
     debate_gate_ph     = st.empty()
     fact_checker_ph    = st.empty()
